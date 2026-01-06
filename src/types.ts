b4a154a26ce87abcd5edbc0198336e3ed09eb8e0
greg archer <nihlton@gmail.com>
2026-01-06 10:38:26 -0800

    fix font-sizes

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import type * as React from 'react';
 import type { DimensionSize, DisplayValue, FontSize, TextAlignValue } from './util';
 type ColumnNumber = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
 type OffsetNumber = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11';
